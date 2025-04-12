18604f5cbed14cc9fdfb41a993761e34839d7b89
Carlos Franco <81891049+Carlosjfranco@users.noreply.github.com>
2025-04-12 14:27:59 -0700

    Update app.py

--- app.py
+++ app.py
@@ -62,7 +62,3 @@ def save_to_db(data):
     ))
     conn.commit()
     conn.close()
-
-if __name__ == '__main__':
-    port = int(os.environ.get('PORT', 5000))
-    app.run(debug=False, host='0.0.0.0', port=port)
